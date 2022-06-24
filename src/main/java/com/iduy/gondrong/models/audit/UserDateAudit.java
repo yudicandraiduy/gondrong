50f827e8a74d5860156029315806442a609b9fb4
yudi candra <yudicandra159@gmail.com>
2022-06-25 03:12:19 +0700

    Penambahan Auditing

---
+++ src/main/java/com/iduy/gondrong/models/audit/UserDateAudit.java
@@ -0,0 +1,42 @@
+package com.iduy.gondrong.models.audit;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class UserDateAudit implements Serializable {
+
+    @CreationTimestamp
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
+
+    @Column(name = "created_by", nullable = false, updatable = false)
+    private Long createdBy;
+
+    @Column(name = "updated_by")
+    private Long updatedBy;
+
+    @PrePersist
+    public void prePersist() {
+        this.setCreatedDate(LocalDateTime.now());
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.setUpdatedDate(LocalDateTime.now());
+    }
+}
